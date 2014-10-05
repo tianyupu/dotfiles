c2f0572d64cd25e36551a66b2614fc74ceb33747
Tian Pu <divine.infinity@gmail.com>
2014-10-05 21:40:39 +1100

    Added basic setup script for dotfiles

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Script to set up dependencies for dotfiles.
+
+# Download packages
+sudo apt-get update
+sudo apt-get install -y \
+  redshift \
+  xmonad xmobar \
+  vim \
+  thunar xscreensaver scrot feh amixer xbacklight

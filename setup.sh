6e661b77746ee6830fbefdb9820520f77bb12e5e
Tian Pu <divine.infinity@gmail.com>
2014-10-25 17:27:46 +1100

    Update install script to include git and tmux
    Uninstall gnome-screensaver if it exists

--- setup.sh
+++ setup.sh
@@ -7,5 +7,10 @@ sudo apt-get update
 sudo apt-get install -y \
   redshift \
   xmonad xmobar suckless-tools \
-  vim \
+  vim git tmux \
   thunar xscreensaver scrot feh amixer xbacklight
+
+# Remove gnome-screensaver if installed
+if [ -e /usr/bin/gnome-screensaver ]; then
+  sudo apt-get remove -y gnome-screensaver
+fi
